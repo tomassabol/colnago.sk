f04e2dcf580de18a2902629e83f9442019a1e423
Tomáš Sabol <tomassabol2@gmail.com>
2023-08-06 15:23:21 +0200

    wip

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,27 +3,39 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer className="flex justify-between w-screen h-[45vh] bg-black">
-        <div className="flex flex-col justify-center items-start w-1/4 h-full">
-          <h3 className="text-[#b59251] text-sm uppercase tracking-widest">
-            Kola
+      <footer className="flex justify-evenly items-start w-screen h-[35vh] bg-black text-sm px-44 py-16">
+        {/* bicykle */}
+        <div className="flex flex-col items-start h-fit gap-6">
+          <h3 className="text-[#b59251] uppercase tracking-widest">Bicykle</h3>
+          <ul className="flex flex-col gap-y-2 text-[#787878]">
+            <li>Cestní</li>
+            <li>Allroad</li>
+            <li>Gravel</li>
+            <li>Časovka</li>
+            <li>Klasika</li>
+          </ul>
+        </div>
+
+        {/* Colnago */}
+        <div className="flex flex-col items-start h-fit gap-6">
+          <h3 className="text-[#b59251] uppercase tracking-widest">
+            Sieň slávy
           </h3>
-          <ul className="flex flex-col gap-y-2">
-            <li className="text-[#b59251] text-sm uppercase tracking-widest">
-              C64
-            </li>
-            <li className="text-[#b59251] text-sm uppercase tracking-widest">
-              V3RS
-            </li>
-            <li className="text-[#b59251] text-sm uppercase tracking-widest">
-              V3RS Disc
-            </li>
-            <li className="text-[#b59251] text-sm uppercase tracking-widest">
-              C64 Disc
-            </li>
-            <li className="text-[#b59251] text-sm uppercase tracking-widest">
-              C64 Disc Brake
-            </li>
+          <ul className="flex flex-col gap-y-2 text-[#787878]">
+            <li>Sieň slávy</li>
+            <li>História</li>
+            <li>Eventy</li>
+          </ul>
+        </div>
+
+        {/* Kontaktujte nás */}
+        <div className="flex flex-col items-start  h-fit gap-6">
+          <h3 className="text-[#b59251] uppercase tracking-widest">
+            Kontaktujte nás
+          </h3>
+          <ul className="flex flex-col gap-y-2 text-[#787878]">
+            <li>Predajne</li>
+            <li>Napíšte nám</li>
           </ul>
         </div>
       </footer>
