b7125b1d86e94d9e31c88782ebe28c8f64d0629d
Tomáš Sabol <tomassabol2@gmail.com>
2023-09-03 10:57:18 +0200

    wip

--- src/app/bicykle/[slug]/page.tsx
+++ src/app/bicykle/[slug]/page.tsx
@@ -2,7 +2,6 @@ import Cyclist from "~/assets/images/z9b_2583_optimized.jpg";
 import BikeDescription from "~/components/BikeDetails/BikeDescription";
 import BikeHeroPage from "~/components/BikeDetails/BikeHeroPage";
 import BikeVideo from "~/components/BikeDetails/BikeVideo";
-import BreadCrumbNav from "~/components/BreadCrumbNav";
 import Spacer from "~/components/Spacer";
 import { trpc } from "~/trpc/serverClient";
 
@@ -26,24 +25,6 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const bike = await trpc.bikes.getBikeBySlug({ slug: params.slug });
   return (
     <>
-      <section
-        className="text-white, h-[85vh] w-full"
-        style={{
-          background: `url('https://s3.eu-west-1.amazonaws.com/colnago.sk/c68/c63_bck.jpg') center 100% no-repeat `,
-        }}
-      >
-        <div className="relative h-full w-screen">
-          <div className="absolute bottom-[30vh] space-y-4 md:w-1/2 xl:left-20 xl:w-1/4">
-            <BreadCrumbNav />
-            <div className="space-y-10 pb-10">
-              <p className="text-5xl font-extrabold italic">
-                {bike?.bike?.name}
-              </p>
-              <p>{bike?.bike?.description}</p>
-            </div>
-          </div>
-        </div>
-      </section>
       {bike?.bike ? <BikeHeroPage bike={bike?.bike} /> : null}
 
       <section className="w-screen space-y-5 bg-slate-50 p-10 text-slate-950 ">
