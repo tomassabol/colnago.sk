484129e3dab0b0b60a4f4e02229ad7587c43692d
Tomáš Sabol <tomassabol2@gmail.com>
2023-09-18 21:21:45 +0200

    wip

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Logo from "~/assets/images/colnago-logo.svg";
+export default function Loading() {
+  return (
+    <>
+      <div className=" flex h-screen w-screen flex-col items-center justify-center">
+        <Image
+          src={Logo}
+          alt="Colnago Logo"
+          className="h-content-fit w-[8em]"
+        />
+        <i>Loading contents...</i>
+      </div>
+    </>
+  );
+}
